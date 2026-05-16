85d2805e5a2e1f086ccb20f7a850bcb1b90a817e
VietHaDuong <launchlou1@gmail.com>
2026-05-16 23:21:38 +0800

    add directory function in main.py

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import os
-# from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession
+import utils.bronze_preprocess as bp
 
-# def create_session():
-#     spk = SparkSession.builder \
-#         .appName('Table_preprocessing') \
-#         .getOrCreate()
-#     return spk
+def create_session():
+    spk = SparkSession.builder \
+        .appName('Table_preprocessing') \
+        .getOrCreate()
+    return spk
 
-PATH = "./data/lms_loan_daily.csv"
 
-print(os.path.basename(PATH))
-print(os.path.splitext(PATH))
+bronze_dir = 'datamart/bronze'
+if not os.path.exists(bronze_dir):
+    os.makedirs(bronze_dir)
 
-print(os.path.splitext(os.path.basename(PATH)))
+silver_dir = 'datamart/silver'
+if not os.path.exists(silver_dir):
+    os.makedirs(silver_dir)
+
+gold_dir = 'datamart/gold'
+if not os.path.exists(gold_dir):
+    os.makedirs(gold_dir)
+
+# PATH = "./data/lms_loan_daily.csv"
+
+# print(os.path.basename(PATH))
+# print(os.path.splitext(PATH))
+
+# print(os.path.splitext(os.path.basename(PATH))[0])
