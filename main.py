a29c21fa937cd8f94d475dfbac7c57640c12e98b
VietHaDuong <launchlou1@gmail.com>
2026-05-17 02:51:12 +0800

    add a bit more to main.py

--- main.py
+++ main.py
@@ -2,28 +2,29 @@ import os
 from pyspark.sql import SparkSession
 import utils.bronze_preprocess as bp
 
-def create_session():
-    spk = SparkSession.builder \
-        .appName('Table_preprocessing') \
-        .getOrCreate()
-    return spk
+# def create_session():
+#     spk = SparkSession.builder \
+#         .appName('Table_preprocessing') \
+#         .getOrCreate()
+#     return spk
 
 
-bronze_dir = 'datamart/bronze'
-if not os.path.exists(bronze_dir):
-    os.makedirs(bronze_dir)
 
-silver_dir = 'datamart/silver'
-if not os.path.exists(silver_dir):
-    os.makedirs(silver_dir)
+# bronze_dir = 'datamart/bronze'
+# if not os.path.exists(bronze_dir):
+#     os.makedirs(bronze_dir)
 
-gold_dir = 'datamart/gold'
-if not os.path.exists(gold_dir):
-    os.makedirs(gold_dir)
+# silver_dir = 'datamart/silver'
+# if not os.path.exists(silver_dir):
+#     os.makedirs(silver_dir)
 
-# PATH = "./data/lms_loan_daily.csv"
+# gold_dir = 'datamart/gold'
+# if not os.path.exists(gold_dir):
+#     os.makedirs(gold_dir)
 
-# print(os.path.basename(PATH))
-# print(os.path.splitext(PATH))
+PATH = "./data"
 
-# print(os.path.splitext(os.path.basename(PATH))[0])
+date = "2024-06-01"
+
+for file in os.listdir(PATH):
+    print(os.path.splitext(os.path.basename(file))[0]+'_'+date)
